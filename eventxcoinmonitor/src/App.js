0cc27cea478f08d430ac73b84454dde4fd3c3961
handsome <handsome@gmail.com>
2022-08-11 11:50:15 +0800

    added: initialized react frontend

---
+++ eventxcoinmonitor/src/App.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react'
+import axios from 'axios'
+import './App.css'
+import Monitor from './monitor/Monitor.tsx'
+
+function App() {
+  useEffect(() => {
+    axios.get('http://localhost:3001', { headers: {} }).then(console.log)
+  }, [])
+  return (
+    <div className="App">
+      <Monitor />
+    </div>
+  )
+}
+
+export default App
---
+++ eventxcoinmonitor/src/monitor/Monitor.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Monitor = () => {
+  return <div>11111</div>
+}
+
+export default Monitor
